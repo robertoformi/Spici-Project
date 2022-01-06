44bee79796f82195f98bd708bc504204faf8c918
robertoformi <95297942+robertoformi@users.noreply.github.com>
2022-01-06 02:12:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, session
-import pymongo
+from flask import Flask, render_template, request, redirect, flash, session, url_for
 from pymongo import MongoClient
 from crypting import checkingPassword, codingPassword
 
@@ -16,11 +15,11 @@ def login():
         password = request.form['pw']
 
         if not(email_is_already_stored(email)):
-            flash("Email not found", "error")      
+            flash("Email Not Found!", "error")      
             return redirect('/')
         else:
             if(checkingPassword(password, password_finder(email))):
-                session["name"] = user_finder(email)         # These two lines of code are needed for presenting a table with the user information in the logged_in page. Check the logged_in function (you will see that both name and email are needed).
+                session["name"] = name_finder(email)         # These two lines of code are needed for presenting a table with the user information in the logged_in page. Check the logged_in function (you will see that both name and email are needed).
                 session["email"] = email                   
                 return redirect('/logged_in')
             else:
@@ -30,17 +29,16 @@ def login():
         return render_template('login.html')
 
 
-@app.route('/subscribe', methods=['GET', 'POST']) 
+@app.route('/subscribe.html', methods=['GET', 'POST']) 
 def subscribe():
     
     if request.method =='POST':
-        name = request.form['username']
+        name = request.form['name']
         surname = request.form['surname']
         email = request.form['email']
         ps1 = request.form['password1']
         ps2 = request.form['password2']
         age = request.form['age']
-
         fiscal_Code = request.form['fiscal_code']
         phone_num = request.form['phone_num']
         company_name = request.form['company_name']
@@ -51,31 +49,33 @@ def subscribe():
         zip = request.form['zip']
         share_capital = request.form['share_capital']
         vat = request.form['vat']
+        country = request.form['country']
         
         
         if email_is_already_stored(email):
             flash("Try with another email: the email is already stored", "error")   
             return redirect('/subscribe')
         else:
-            data = {"name": name,
-                    "surname": surname,
+            data = {"Name": name,
+                    "Surname": surname,
                     "email": email, 
                     "ps": codingPassword(ps1),
-                    "age": age,
-                    "fiscal_code": fiscal_Code,
-                    "phone_num": phone_num,
-                    "company_name": company_name,
-                    "purpose": purpose,
-                    "date_foundation": date_foundation,
-                    "city": city,
-                    "street": street,
-                    "zip": zip,
-                    "share_capital": share_capital,
-                    "vat": vat,
+                    "Age": age,
+                    "Fiscal Code": fiscal_Code,
+                    "Phone Number": phone_num,
+                    "Company's Name": company_name,
+                    "Company's Purpose": purpose,
+                    "Date of Foundation": date_foundation,
+                    "City": city,
+                    "Street": street,
+                    "ZIP Code": zip,
+                    "Share Capital": share_capital,
+                    "VAT Code": vat,
+                    "Country": country
                     }
 
             collection.insert_one(data)
-        return redirect('/greet')
+        return redirect(url_for('greet', email = email))
 
     else:
         return render_template('/subscribe.html')
@@ -97,9 +97,9 @@ def logout():
     # It is important because when someone press the logout button, then the session with its name should not be accessible, by typing the path, anymore.
     return redirect("/")
 
-@app.route('/greet')
-def greet():
-    return render_template("greet.html")
+@app.route('/greet/<email>')
+def greet(email):
+    return render_template("greet.html", doc = data_finder(email))
 
 def email_is_already_stored(email):
     '''Returns True if email is already in the database, False if not'''
@@ -109,12 +109,12 @@ def email_is_already_stored(email):
 # collection.find_one({"email": email}) == None  returns False. So not(False)=True. So, if the email is in the
 # database, this function returns True.
 
-def user_finder(email): 
+def name_finder(email): 
     '''Returns the username stored according to the email inserted'''
     cur = collection.find({"email": email})   # Since we use .find and not .find_one, the result is a cursor
     # that basically returns all the dictionaries with the email equal to the email we selected.
     for doc in cur:  
-        return doc["user"]
+        return doc["name"]
 
 def password_finder(email):
     '''Returns the hashed password stored according to the email inserted'''
@@ -125,10 +125,12 @@ def password_finder(email):
     return rescued_hashed_password
 
 def data_finder(email): 
+
     '''Returns all the user information but id and pw'''
     cur = collection.find({"email": email}) 
     for doc in cur:
         data_found = doc
+    data_found["Subscribed since: "] = str(data_found["_id"].generation_time).split()[0]
     del data_found["_id"]   
     del data_found["ps"]    
     cur.close()
